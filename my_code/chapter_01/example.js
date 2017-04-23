10c0b870b5f4bc7e16e81e6d0f48b02e9224621e
arthurlee <boyal2008@gmail.com>
2017-04-23 22:25:29 +0800

    refactoring

--- my_code/chapter_01/example.js
+++ my_code/chapter_01/example.js
@@ -7,12 +7,16 @@ context.font = '38pt Arial';
 context.fillStyle = 'cornflowerblue';
 context.strokeStyle = 'cornflowerblue';
 
+const text = 'Hello Canvas';
+let x = canvas.width/2 - 150;
+let y = canvas.height/2 + 15;
+
 // TODO: 更好的居中文字的方式（在第三章中）
-context.fillText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
+context.fillText(text, x, y);
 //context.strokeText('Hello Canvas', canvas.width/2 - 150, canvas.height/2 + 15);
 
 // 立体效果
 //context.strokeText('Hello Canvas', canvas.width/2 - 148, canvas.height/2 + 17);
 
 // 更有质感
-context.strokeText('Hello Canvas', canvas.width/2 - 152, canvas.height/2 + 13);
+context.strokeText(text, x - 2, y - 2);
